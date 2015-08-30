db18a400e3cb9a1a7a4ea8af1c7dade16ba775c1
Nevena Djaja <nevena.djaja@gmail.com>
2015-08-30 16:36:34 -0700

    fixed the bug with twoSum and updated tests

--- examples/twosum.js
+++ examples/twosum.js
@@ -3,14 +3,15 @@ function twosum(arr, sum) {
 	for (var ind=0; ind<arr.length; ind++) {
 		var elem = arr[ind];
 		if (found[sum-elem]) {
-			return [found[sum-elem], ind];
+			return [found[sum-elem]-1, ind];
 		} else {
-			found[elem] = ind;
+			found[elem] = ind+1;
 		}
 	}
 }
 
 arr = [1,3,5,2,6,8,9];
 console.log(twosum(arr, 10));
+console.log(twosum([1,3,2],3));
 
 exports.twosum = twosum;
--- tests/twosum_spec.js
+++ tests/twosum_spec.js
@@ -1,8 +1,13 @@
 var twosum = require('../examples/twosum');
 
 describe("twosum#twosum", function() {
-	it ("should [3,5]", function() {
+	it ("should return [3,5]", function() {
 		var result = twosum.twosum([1,3,5,2,6,8,9], 10);
 		expect(result).toEqual([3,5]);
+	});
+
+	it ("should return [0,2]", function() {
+		var result = twosum.twosum([1,3,2], 3);
+		expect(result).toEqual([0,2]);
 	});
 });
